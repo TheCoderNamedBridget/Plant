186c2a4dac0cdf808fc505aaac50756bf338c157
Bridget <bridgetnaylor2000@gmail.com>
2020-02-04 10:29:15 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/plant/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.plant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Have fun mini games to help people learn plant info
+//have review cards for plant info
+//have a mini explore feature to let players "explore" a mini landscape
+//have a connect to PlantWorld account -> send rewards
+// have a screen to keep track of the rewards sent
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
